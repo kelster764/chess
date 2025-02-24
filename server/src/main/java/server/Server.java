b81e902281ef725aad1f40f971d15ca7a045d6e9
kelster764 <kpharmon@byu.edu>
2025-02-24 15:55:07 -0700

    did register service

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,18 +1,24 @@
 package server;
 
+import com.google.gson.Gson;
 import dataaccess.*;
 //import dataaccess.MemoryDataAccess;
 import dataaccess.MemoryGameDao;
 import dataaccess.MemoryAuthAccess;
 import dataaccess.MemoryUserAccess;
+//import model.AuthData;
+import model.*;
 import service.ClearService;
+import service.RegisterService;
 import spark.*;
 
 public class Server {
 //    private final loginService;
 //    private final registerService;
 //    private final joinGameService;
+    //private final UserData userData;
     private final ClearService clearService;
+    private final RegisterService registerService;
     private final GameDAO gameDao;
     private final AuthDAO authDao;
     private final UserDAO userDao;
@@ -20,11 +26,13 @@ public class Server {
     //private final WebSocketHandler webSocketHandler;
 
     public Server() {
-        //this.dataAccess = new MemoryDataAccess();
+        //this.dataAccess =
+        //this.userData == new UserData()
         this.gameDao = new MemoryGameDao();
         this.authDao = new MemoryAuthAccess();
         this.userDao = new MemoryUserAccess();
         this.clearService = new ClearService(gameDao, authDao, userDao);
+        this.registerService = new RegisterService(authDao, userDao);
 //        this.LoginService = loginService;
 //        this.RegisterService = registerService;
 //        this.JoinGameService = joinGameService;
@@ -38,6 +46,8 @@ public class Server {
         Spark.staticFiles.location("web");
 
         Spark.delete("/db", this::clear);
+
+        Spark.post("/user", this::register);
 
         // Register your endpoints and handle exceptions here.
 
@@ -59,6 +69,38 @@ public class Server {
         clearService.clear();
         res.status(200);
         return "{}";
+    }
+
+    private Object register(Request req, Response res) throws DataAccessException{
+        String body = req.body();
+        UserData jbody = new Gson().fromJson(body, UserData.class);
+        try {
+            AuthData authdata = registerService.RegisterUser(jbody);
+            String jauth = new Gson().toJson(authdata);
+            res.status(200);
+            return jauth;
+        } catch(Exception ex){
+            if (ex.getMessage().equals("Error: already taken")){
+                res.status(403);
+                res.body(ex.getMessage());
+                return new Gson().toJson(ex.getMessage());
+            }
+            else if(ex.getMessage().equals("Error: bad request")){
+                res.status(400);
+                res.body(ex.getMessage());
+                return new Gson().toJson(ex.getMessage());
+            }
+            else{
+                res.status(500);
+                res.body(ex.getMessage());
+                return new Gson().toJson(ex.getMessage());
+            }
+        }
+
+            //{ "username":"", "password":"", "email":"" }
+//        res.status(200);
+//
+//        return jauth;
     }
 
 }
--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,4 +1,30 @@
 package service;
 
-public class RegisterService {
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import dataaccess.DataAccessException;
+import model.*;
+
+public class RegisterService  {
+    private final AuthDAO authDao;
+    private final UserDAO userDao;
+
+    public RegisterService(AuthDAO authDao, UserDAO userDao){
+        this.authDao = authDao;
+        this.userDao = userDao;
+    }
+    public AuthData RegisterUser(UserData userData) throws DataAccessException {
+        if (userDao.getUser(userData.username()) != null) {
+                throw new DataAccessException("Error: already taken") ;
+        }
+        if (userData.username() == null || userData.password() == null || userData.email() == null){
+            throw new DataAccessException("Error: bad request") ;
+        }
+        //create actual auth data
+        userDao.createUser(userData);
+        AuthData authData = authDao.createAuth(userData.username());
+        return authData;
+    }
+
 }
