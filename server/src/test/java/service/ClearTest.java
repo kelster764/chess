a47d5c612fedaab73a4c14856e4e95020e84a8aa
kelster764 <kpharmon@byu.edu>
2025-02-28 14:49:40 -0700

    list games tests

--- server/src/test/java/service/ClearTest.java
+++ server/src/test/java/service/ClearTest.java
@@ -10,11 +10,20 @@ import org.junit.jupiter.api.*;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+
 public class ClearTest {
     MemoryAuthAccess authAccess = new MemoryAuthAccess();
     MemoryGameDao  gameAccess = new MemoryGameDao();
     MemoryUserAccess userAccess = new MemoryUserAccess();
     ClearService clearService = new ClearService(gameAccess, authAccess, userAccess);
+    RegisterService registerService = new RegisterService(authAccess, userAccess);
+    //loginService loginService = new LoginService(authDao, userDao);
+//        this.logoutService = new LogoutService(authDao);
+//        this.addGameService = new AddGameService(authDao, gameDao);
+    ListGameService listGameService = new ListGameService(authAccess, gameAccess);
+//        this.joinGameService = new JoinGameService(authDao, gameDao);
+
     //AuthData authData = new RegisterService.
     String userName = "yomama";
     String password = "ishot";
@@ -24,6 +33,7 @@ public class ClearTest {
     UserData userData = new UserData(userName, password, email);
     GameData gameData = new GameData(1, userName, "Yourmom", "thegame", game);
     //AuthData authData = new MemoryAuthAccess().createAuth(userName);
+
 
     @Test
     @DisplayName("test clearing")
@@ -43,6 +53,31 @@ public class ClearTest {
         }
 
         Assertions.assertTrue(userAccess.users.isEmpty());
+        Assertions.assertTrue(gameAccess.chessGames.isEmpty());
+        Assertions.assertTrue(authAccess.authentications.isEmpty());
 
     }
+    @Test
+    public void listGames(){
+        Assertions.assertTrue(gameAccess.chessGames.isEmpty());
+        gameAccess.createGame(gameData);
+        AuthData auth = authAccess.createAuth(userName);
+        try {
+            Collection<GameData> games1= listGameService.listGame(auth.authToken());
+            Collection<GameData> games2 = gameAccess.listGames();
+            Assertions.assertSame(games1, games2);
+        }catch(Exception ex){
+            Assertions.fail();
+        }
+
+    }
+    @Test
+    public void listGamesFail(){
+        try {
+            listGameService.listGame("totally wrong authtoken");
+        }catch(Exception ex){
+            Assertions.assertInstanceOf(DataAccessException.class, ex);
+        }
+    }
+
 }
