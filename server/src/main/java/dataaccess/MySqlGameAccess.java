50526816661260b9afcb16e44898a08ff3eb9cf2
kelster764 <kpharmon@byu.edu>
2025-03-10 16:53:00 -0600

    time to test and debug... oh boy

--- server/src/main/java/dataaccess/MySqlGameAccess.java
+++ server/src/main/java/dataaccess/MySqlGameAccess.java
@@ -103,7 +103,7 @@ public void updateGame(int gameID, String whiteUsername, String blackUsername, S
                     var param = params[i];
                     if (param instanceof String p) ps.setString(i + 1, p);
                     else if (param instanceof Integer p) ps.setInt(i + 1, p);
-                    else if (param instanceof ChessGame p) ps.setString(i + 1, p);
+                    //else if (param instanceof ChessGame p) ps.setString(i + 1, p);
                     else if (param == null) ps.setNull(i + 1, NULL);
                 }
                 ps.executeUpdate();
