a2134997e57c4203c1abdc329c227020ad719f78
kelster764 <kpharmon@byu.edu>
2025-03-10 13:29:35 -0600

    still working on gamsql

--- server/src/main/java/dataaccess/MySqlGameAccess.java
+++ server/src/main/java/dataaccess/MySqlGameAccess.java
@@ -20,9 +20,56 @@ public class MySqlGameAccess implements GameDAO{
         configureDatabase();
     }
 
+    public GameData createGame(GameData game) throws DataAccessException {
+        var statement = "INSERT INTO gameData (whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?, ?)";
+        var json = new Gson().toJson(game.game());
+        executeUpdate(statement, game.whiteUsername(), game.blackUsername(), game.gameName(), json);
+        return game;
+    }
 
+    public void clearGames() throws DataAccessException {
+        var statement = "TRUNCATE gameData";
+        executeUpdate(statement);
+    }
+    public GameData getGame(int gameID) throws DataAccessException {
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT whiteUsername, blackUsername, gameName, game FROM gameData WHERE gameID=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (var rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readGame(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return null;
+    }
 
+    private void executeUpdate(String statement, Object... params) throws DataAccessException{
+        try (var conn = DatabaseManager.getConnection()) {
+            try (var ps = conn.prepareStatement(statement)) {
+                for (var i = 0; i < params.length; i++) {
+                    var param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
 
+//                var rs = ps.getGeneratedKeys();
+//                if (rs.next()) {
+//                    //return rs.getInt(1);
+//                }
+//
+//                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 
     public final String[] createStatements = {
 //I need to figure out how to put the game object into the thing
@@ -31,11 +78,13 @@ public class MySqlGameAccess implements GameDAO{
                `gameID` int NOT NULL AUTO_INCREMENT,
                `whiteUsername` varchar(256),
                `blackUsername` varchar(256),
-               `gameName` varchar(256) NOT NULL;
+               `gameName` varchar(256) NOT NULL,
+               `game` TEXT DEFAULT NULL,
                 PRIMARY KEY (`gameID`),
                 INDEX(whiteUsername),
                 INDEX(blackUsername),
                 INDEX(gameName),
+                INDEX(game),
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
