f31163f37b06d1b6ce2eb778f3635bb9f0c835a2
kelster764 <kpharmon@byu.edu>
2025-03-17 16:45:53 -0600

    initialized things

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,10 +1,23 @@
 package server;
+import model.*;
+import dataaccess.*;
+
+import java.net.*;
 
 public class ServerFacade {
 
     private final String serverUrl;
 
-    public ServerFacade(String url) { serverUrl = url;} }
+    public ServerFacade(String url) { serverUrl = url;}
 
+    public UserData register(UserData userData){
+        var path = "/user";
+        return this.makeRequest("POST", path, userData, UserData.class);
+    }
+
+
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws DataAccessException{
+
+    }
 
 }
