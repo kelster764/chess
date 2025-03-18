fc7dc2fab644957243e02b8a68aca8cd6bdda7d4
kelster764 <kpharmon@byu.edu>
2025-03-18 16:21:17 -0600

    typed out rest of ServerFacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -4,6 +4,7 @@ import model.*;
 import exception.DataAccessException;
 
 import javax.xml.crypto.Data;
+import java.awt.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -19,36 +20,53 @@ public class ServerFacade {
     public AuthData register(String username, String password, String email) throws DataAccessException {
         var path = "/user";
         UserData user = new UserData(username, password, email);
-        return this.makeRequest("POST", path, user, AuthData.class);
+        return this.makeRequest("POST", path, user, null,  AuthData.class);
     }
 
     public AuthData login(String username, String password) throws DataAccessException{
         var path = "/session";
         UserData user = new UserData(username, password, null);
-        return this.makeRequest("POST", path, user, AuthData.class);
+        return this.makeRequest("POST", path, user, null, AuthData.class);
     }
 
     public void logout(String authToken) throws DataAccessException{
         var path = "/session";
-        this.makeRequest("DELETE", path, null, null);
+        this.makeRequest("DELETE", path, null, authToken, null);
     }
 
-    public GameData createGame(String gameName) throws DataAccessException{
+    public GameData createGame(String gameName, String authToken) throws DataAccessException{
         var path = "/game";
-        GameData game = new GameData()
+        GameData game = new GameData(0, null, null, gameName, null);
+        return this.makeRequest("POST", path, game, authToken, GameData.class);
+    }
+    
+    public GameData[] listGames(String authToken) throws DataAccessException{
+        var path = "/game";
+        record listGameResponse(GameData[] game){
+        }
+        var response = this.makeRequest("GET", path, null, authToken, listGameResponse.class);
+        return response.game();
+    }
+
+    public void joinGame(int gameID, String color, String authToken) throws DataAccessException{
+        var path = "/game";
+        ColorData colorData = new ColorData(color, gameID);
+        this.makeRequest("PUT", path, colorData, authToken, null);
     }
 
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws DataAccessException{
+    private <T> T makeRequest(String method, String path, Object request, String header, Class<T> responseClass) throws DataAccessException{
         try{
             URL url = (new URI(serverUrl + path)).toURL();
             HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod(method);
             http.setDoOutput(true);
 
+            writeHeader(header, http);
+
             writeBody(request, http);
             http.connect();
-            //throwIfNotSuccessful(http);
+            throwIfNotSuccessful(http);
             return readBody(http, responseClass);
         }catch(Exception ex){
             throw new DataAccessException(ex.getMessage());
@@ -70,15 +88,18 @@ public class ServerFacade {
     }
 
 
-//    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, DataAccessException {
-//        var status = http.getResponseCode();
-//        if (!isSuccessful(status)) {
-//            try (InputStream respErr = http.getErrorStream()) {
-//                }catch(Exception ex){
-//                throw new DataAccessException(ex.getMessage());
-//            }
-//        }
-//    }
+    private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, DataAccessException {
+        var status = http.getResponseCode();
+        if (!isSuccessful(status)) {
+            try (InputStream respErr = http.getErrorStream()) {
+                if (respErr != null) {
+                    throw new DataAccessException("not null");
+                }
+                }catch(Exception ex){
+                throw new DataAccessException(ex.getMessage());
+            }
+        }
+    }
 
     private boolean isSuccessful(int status) {
         return status / 100 == 2;
@@ -91,6 +112,11 @@ public class ServerFacade {
             try (OutputStream reqBody = http.getOutputStream()) {
                 reqBody.write(reqData.getBytes());
             }
+        }
+    }
+    private static void writeHeader(String header, HttpURLConnection http){
+        if (header != null) {
+            http.addRequestProperty("authorization", header);
         }
     }
 
