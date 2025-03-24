3ca1725f5739c10068b1666f844afd6cf0efb6f4
kelster764 <kpharmon@byu.edu>
2025-03-24 09:38:36 -0600

    starting to make chessboard ui

---
+++ client/src/main/java/ui/ChessUI.java
@@ -0,0 +1,38 @@
+package ui;
+import java.io.PrintStream;
+
+
+public class ChessPrint {
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 1;
+    private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
+
+    public static final String WHITE_KING = " ♔ ";
+    public static final String WHITE_QUEEN = " ♕ ";
+    public static final String WHITE_BISHOP = " ♗ ";
+    public static final String WHITE_KNIGHT = " ♘ ";
+    public static final String WHITE_ROOK = " ♖ ";
+    public static final String WHITE_PAWN = " ♙ ";
+    public static final String BLACK_KING = " ♚ ";
+    public static final String BLACK_QUEEN = " ♛ ";
+    public static final String BLACK_BISHOP = " ♝ ";
+    public static final String BLACK_KNIGHT = " ♞ ";
+    public static final String BLACK_ROOK = " ♜ ";
+    public static final String BLACK_PAWN = " ♟ ";
+    public static final String EMPTY = " \u2003 ";
+
+
+    private static void drawHeaders(PrintStream out) {
+        //setGray(out);
+
+        String[] headers = {"a", "b", "c", "d", "e", "f", "g", "h"};
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            drawHeader(out, headers[boardCol]);
+
+            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
+                out.print(EMPTY.repeat(LINE_WIDTH_IN_PADDED_CHARS));
+            }
+        }
+    }
+
+}
