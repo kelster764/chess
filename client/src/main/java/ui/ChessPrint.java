a41ddff42f609c8410e5294f97a11d15b264b4b7
kelster764 <kpharmon@byu.edu>
2025-03-24 19:57:11 -0600

    the board is aligned! now to color

--- client/src/main/java/ui/ChessPrint.java
+++ client/src/main/java/ui/ChessPrint.java
@@ -16,12 +16,12 @@ public class ChessPrint {
     public static final String WHITE_KNIGHT = "♘";
     public static final String WHITE_ROOK = "♖";
     public static final String WHITE_PAWN = "♙";
-    public static final String BLACK_KING = "K";
-    public static final String BLACK_QUEEN = "Q";
-    public static final String BLACK_BISHOP = "B";
-    public static final String BLACK_KNIGHT = "N";
-    public static final String BLACK_ROOK = "R";
-    public static final String BLACK_PAWN = "P";
+    public static final String BLACK_KING = " K ";
+    public static final String BLACK_QUEEN = " Q ";
+    public static final String BLACK_BISHOP = " B ";
+    public static final String BLACK_KNIGHT = " N ";
+    public static final String BLACK_ROOK = " R ";
+    public static final String BLACK_PAWN = " P ";
     public static final String EMPTY = " \u2003 ";
 
 
@@ -44,21 +44,21 @@ public class ChessPrint {
     private static void drawHeaders(PrintStream out) {
         //setGray(out);
 
-        String[] headers = {" ","a", "b", "c", "d", "e", "f", "g", "h", " "};
+        String[] headers = {"   "," a ", " b ", " c ", " d ", " e ", " f ", " g ", " h ", "   "};
+        //out.print(EMPTY.repeat(1));
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
             drawHeader(out, headers[boardCol]);
         }
+        setDef(out);
         out.println();
     }
     private static void drawHeader(PrintStream out, String headerText) {
-//        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-//        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
-        int prefixLength = 1;
-        int suffixLength = 1;
+        int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+        int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
-        out.print(EMPTY.repeat(prefixLength));
+        //out.print(EMPTY.repeat(prefixLength));
         printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
+        //out.print(EMPTY.repeat(suffixLength));
     }
 
     private static void printHeaderText(PrintStream out, String player) {
@@ -73,35 +73,54 @@ public class ChessPrint {
     private static void drawChessBoard(PrintStream out) {
 
         for (int boardRow = 1; boardRow < BOARD_SIZE_IN_SQUARES-1; ++boardRow) {
-
+            setWhite(out);
+            //out.print(EMPTY.repeat(1));
             drawRowOfSquares(out, boardRow);
 
-//            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
-//                // Draw horizontal row separator.
-//                drawHorizontalLine(out);
-//                setBlack(out);
-//            }
+            if (boardRow < BOARD_SIZE_IN_SQUARES - 1) {
+                // Draw horizontal row separator.
+                //drawHorizontalLine(out);
+                setBlack(out);
+            }
         }
     }
 
     private static void drawRowOfSquares(PrintStream out, int boardRow) {
-        String[] blackPieces = {String.valueOf(boardRow),BLACK_ROOK,BLACK_BISHOP,BLACK_KNIGHT,BLACK_QUEEN,BLACK_KING,
-                BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, String.valueOf(boardRow)};
+        String rowLabel = String.format(" "+String.valueOf(boardRow+ " "));
+        String[] blackPieces = {rowLabel,BLACK_ROOK,BLACK_BISHOP,BLACK_KNIGHT,BLACK_QUEEN,BLACK_KING,
+                BLACK_BISHOP, BLACK_KNIGHT, BLACK_ROOK, rowLabel};
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
                 setWhite(out);
-//                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
-//                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
-                double prefixLength = .3;
-                double suffixLength = .3;
+                    int prefixLength = SQUARE_SIZE_IN_PADDED_CHARS / 2;
+                    int suffixLength = SQUARE_SIZE_IN_PADDED_CHARS - prefixLength - 1;
 
-                    out.print(EMPTY.repeat(prefixLength));
+                    //out.print(EMPTY.repeat(prefixLength));
                     printPlayer(out, blackPieces[boardCol]);
-                    out.print(EMPTY.repeat(suffixLength));
+                    //out.print(EMPTY.repeat(suffixLength));
                 }
 
-                setBlack(out);
+                //setBlack(out);
+            setDef(out);
 
             out.println();
+    }
+
+    private static void drawHorizontalLine(PrintStream out) {
+
+//        int boardSizeInSpaces = (2 * SQUARE_SIZE_IN_PADDED_CHARS +
+//                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_PADDED_CHARS)-1;
+        int boardSizeInSpaces = 14;
+
+
+        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_PADDED_CHARS; ++lineRow) {
+            setWhite(out);
+            out.print(EMPTY.repeat(boardSizeInSpaces));
+            //out.print("");
+
+            //setBlack(out);
+            setDef(out);
+            out.println();
+        }
     }
 
 //    private static void drawRowOfSquares(PrintStream out, int boardRow) {
@@ -159,6 +178,11 @@ public class ChessPrint {
     private static void setBlack(PrintStream out) {
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_BLACK);
+    }
+
+    private static void setDef(PrintStream out) {
+        out.print(RESET_BG_COLOR);
+        out.print(RESET_TEXT_COLOR);
     }
 
 
