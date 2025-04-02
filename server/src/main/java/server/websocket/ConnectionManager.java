11c547489f984e0a6583846ea24b8aa14e203bec
kelster764 <kpharmon@byu.edu>
2025-04-02 14:58:28 -0600

    broadcast

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,8 +1,12 @@
 package server.websocket;
 
 import model.GameData;
-import spark.Session;
+//import spark.Session;
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,11 +14,32 @@ public class ConnectionManager {
     public final ConcurrentHashMap<Integer, Set<Session>> sessionMap= new ConcurrentHashMap<>();
 
     public void addSessionToGame(Integer gameID, Session session){
-        Set<Session >currentSet = sessionMap.getOrDefault(gameID, Set.of());
+        Set<Session>currentSet = sessionMap.getOrDefault(gameID, Set.of());
         currentSet.add(session);
         sessionMap.put(gameID, currentSet);
     }
 
+    public void removeSessionFromGame(Integer gameID, Session session){
+        Set<Session>currentSet = sessionMap.getOrDefault(gameID, Set.of());
+        currentSet.remove(session);
+        sessionMap.put(gameID, currentSet);
+    }
+
+    public Set<Session> getSessionsForGame(Integer gameID){
+        Set<Session>currentSet = sessionMap.getOrDefault(gameID, Set.of());
+        return currentSet;
+    }
+
+    public void broadcast(Integer gameID, Session currentSession, ServerMessage message) throws IOException {
+        var idSessions = getSessionsForGame(gameID);
+        for (Session session : idSessions){
+            if (session.isOpen() && session!= currentSession){
+                String msg = message.toString();
+                session.getRemote().sendString(msg);
+            }
+        }
+
+    }
 
 
 }
