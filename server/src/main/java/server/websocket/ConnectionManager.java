6eba10c5de84f1b6ff822df00d8b6bb2ab2cc704
kelster764 <kpharmon@byu.edu>
2025-04-02 14:12:48 -0600

    addSession

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -9,5 +9,12 @@ import java.util.concurrent.ConcurrentHashMap;
 public class ConnectionManager {
     public final ConcurrentHashMap<Integer, Set<Session>> sessionMap= new ConcurrentHashMap<>();
 
+    public void addSessionToGame(Integer gameID, Session session){
+        Set<Session >currentSet = sessionMap.getOrDefault(gameID, Set.of());
+        currentSet.add(session);
+        sessionMap.put(gameID, currentSet);
+    }
+
+
 
 }
